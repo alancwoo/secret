b474a195b3afcdb667eb37572a0569fb236660fd
Alan Woo <studio@alanwoo.ca>
2021-06-11 16:47:00 +0200

    Production build, adjust 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.default', ['title' => 'Not Found'])
 
 @section('content')
-<div class="text-5xl text-center">
-  🦦
+<div>
+  <div class="mt-3 text-red-400 text-center">The message does not exist, expired, or the key is incorrect. Please try again.</div>
+  <img src="/images/cliparts/{{ rand(1,16) }}.jpg" class="max-w-md mx-auto mt-6" />
 </div>
 @stop
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,13 @@
 let mix = require('laravel-mix')
+require('laravel-mix-purgecss')
+
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        require('tailwindcss'),
-        require('postcss-nested')
-    ])
+    .postCss('resources/css/app.css', 'public/css')
+    .options({
+        postCss: [
+            require('tailwindcss'),
+            require('postcss-nested')
+        ]
+    })
+    .purgeCss()
     .browserSync('http://secret.test')
